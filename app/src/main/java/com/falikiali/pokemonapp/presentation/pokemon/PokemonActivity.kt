ceeda77e8c08264d8df1a74ed55bb751363d8c9b
Ali Faliki <alifaliki456@gmail.com>
2023-10-31 09:07:36 +0700

    Create feature for sorting list pokemon

--- app/src/main/java/com/falikiali/pokemonapp/presentation/pokemon/PokemonActivity.kt
+++ app/src/main/java/com/falikiali/pokemonapp/presentation/pokemon/PokemonActivity.kt
@@ -3,20 +3,28 @@ package com.falikiali.pokemonapp.presentation.pokemon
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.RadioButton
 import androidx.activity.viewModels
 import androidx.appcompat.widget.SearchView
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.falikiali.pokemonapp.R
 import com.falikiali.pokemonapp.databinding.ActivityPokemonBinding
 import com.falikiali.pokemonapp.presentation.detail.DetailPokemonActivity
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PokemonActivity : AppCompatActivity() {
 
+    private var querySearch: String? = null
+
     private val binding: ActivityPokemonBinding by lazy { ActivityPokemonBinding.inflate(layoutInflater) }
     private val viewModel: PokemonViewModel by viewModels()
 
     private lateinit var pokemonAdapter: PokemonAdapter
+    private lateinit var bottomSheetDialog: BottomSheetDialog
+    private lateinit var rbAz: RadioButton
+    private lateinit var rbZa: RadioButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,6 +34,7 @@ class PokemonActivity : AppCompatActivity() {
         initRv()
         observeViewModel()
         searchPokemon()
+        sortData()
     }
 
     private fun setUpActionBar() {
@@ -63,16 +72,51 @@ class PokemonActivity : AppCompatActivity() {
             svSearch.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
                 override fun onQueryTextSubmit(p0: String?): Boolean {
                     viewModel.searchPokemon(p0!!, null)
+                    querySearch = p0
                     return true
                 }
 
                 override fun onQueryTextChange(p0: String?): Boolean {
                     if (p0?.isEmpty() == true) {
                         viewModel.getPokemon(null)
+                        querySearch = null
                     }
                     return true
                 }
             })
+        }
+    }
+
+    private fun sortData() {
+        binding.fabSort.setOnClickListener {
+            val dialogView = layoutInflater.inflate(R.layout.bottom_sheet_filter, null)
+
+            rbAz = dialogView.findViewById(R.id.rb_az)
+            rbZa = dialogView.findViewById(R.id.rb_za)
+
+            bottomSheetDialog = BottomSheetDialog(this@PokemonActivity)
+            bottomSheetDialog.setContentView(dialogView)
+            bottomSheetDialog.show()
+            
+            rbAz.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    if (querySearch != null) {
+                        viewModel.searchPokemon(querySearch!!, 1)
+                    } else {
+                        viewModel.getPokemon(1)
+                    }
+                }
+            }
+
+            rbZa.setOnCheckedChangeListener { _, isChecked ->
+                if (isChecked) {
+                    if (querySearch != null) {
+                        viewModel.searchPokemon(querySearch!!, 2)
+                    } else {
+                        viewModel.getPokemon(2)
+                    }
+                }
+            }
         }
     }
 
